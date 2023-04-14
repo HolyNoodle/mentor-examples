a2d47affebfb288b669a7adc0c091323af17c95d
holynoodle <kbriffaut@gmail.com>
2023-04-14 13:41:29 +0200

    chore: add comments

--- src/functionnal_vs_oop/solution.ts
+++ src/functionnal_vs_oop/solution.ts
@@ -1,13 +1,17 @@
+// Functionnal way
+// Pure typescript: We define the structure of the object, optional but make the code clearer and easier to work with
 interface Ship {
   health: number;
   name: string;
 }
 
+// Equivalent of the constructor in OOP. This is an object creator (not official term)
 const createShip = (name: string): Ship => ({
   name,
   health: 3
 });
 
+// Function that belongs to an object are defined on there own and don't use this. They instead expect a parameter
 const hitShip = (ship: Ship) => {
   ship.health--;
 }
@@ -16,7 +20,7 @@ function hitShips() {
   const ships = [
     createShip("1"),
     createShip("2"),
-    createShip("2"),
+    createShip("3"),
   ];
   for (const ship of ships) {
     hitShip(ship);
